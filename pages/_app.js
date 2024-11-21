d80c8f6191537aa4095bd40c482af89357532b84
DDAK <dawood.khan@gmail.com>
2024-11-21 12:53:36 +0400

    Added posthog for analytics and more

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import { React, Component } from "react";
 import Head from "next/head";
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import "../styles/global.styles.css";
 
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  })
+
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(()=> {
+    const handleRouteChange = () => posthog.capture("$pageview")
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
   return (
     <>
+    <PostHogProvider client={posthog}>
       <Head>
       <meta name="google-adsense-account" content="ca-pub-1570360577499483"/>
         <link
@@ -20,6 +41,7 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       <Component {...pageProps} />
+    </PostHogProvider>  
     </>
   );
 }
